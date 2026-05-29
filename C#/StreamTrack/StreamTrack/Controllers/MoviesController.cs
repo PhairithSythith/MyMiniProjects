d4ff6a384f84c6adfb965e13b5559a7802238fac
PhairithSythith <jank69titanilla@gmail.com>
2026-05-29 12:38:07 +0200

    Automata terv-mentes (2026. 05. 29. 12:38:07,59)

---
+++ C#/StreamTrack/StreamTrack/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StreamTrack.Models;
+
+namespace StreamTrack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static readonly List<Movie> MockMovies = new List<Movie>
+        {
+            new Movie { Id = 1, Title = "Stranger Things", Description = "Rejtélyes dolgok egy kisvárosban.", StreamingPlatform = "Netflix", Type = "Series" },
+            new Movie { Id = 2, Title = "Dűne: Második rész", Description = "Paul Atreides bosszúja.", StreamingPlatform = "HBO Max", Type = "Movie" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> GetMovies()
+        {
+            return Ok(MockMovies);
+        }
+    }
+}
