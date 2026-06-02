a04153aad8cd708de520676511b972bba482213d
PhairithSythith <jank69titanilla@gmail.com>
2026-06-02 13:55:28 +0200

    Automata terv-mentes (2026. 06. 02. 13:55:27,96)

---
+++ In_Progress_Projektek/To-do-list/To_Do/To_Do/wwwroot/jQuery.js
@@ -0,0 +1,38 @@
+$(function () {
+
+    const $ujFeladat = $('#feladatPlusz');
+    const $nincsKesz = $('#nkLista');
+    const $kesz = $('#kLista');
+
+    $ujFeladat.submit(function (e) {
+        e.preventDefault();
+
+        const $input = $('#feladat');
+        const feladat = $input.val().trim();
+
+        if (feladat !== '') {
+
+            const $btnTorol = $("<button type='button'>Törlés</button>");
+            const $btnKezs = $("<button type='button'>Kész</button>");
+            const $nli = $("<li></li>").text(feladat);
+            const $ntarol = $('<div class="ntarol"></div>');
+            const $kli = $("<li></li>").text(feladat);
+            const $ktarol = $('<div class="ktarol"></div>');
+            const $btnArch = $("<button type='button'>Archívumba mentés</button>");
+
+            $ntarol.append($nli).append($btnTorol);
+            $nincsKesz.append($ntarol);
+
+            $input.val('');
+
+            $btnTorol.on('click', function () {
+                $(this).parent().slideUp(300);
+            });
+            $btnKezs.on('click', function () {
+                $(this).parent().slideDown(300);
+
+            }
+        }
+    });
+
+});
