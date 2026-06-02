cf6952473775d750b06157038e5fb0172cf174c3
PhairithSythith <jank69titanilla@gmail.com>
2026-06-02 12:37:38 +0200

    Automata terv-mentes (2026. 06. 02. 12:37:38,50)

--- Adatbázis/Lekérdezés #3.sql
+++
@@ -1,8 +0,0 @@
--- Felhasználói adatokat tartalmazó tábla
-CREATE TABLE Felhasznalo(
-ID intfilm AUTO_INCREMENT PRIMARY KEY,
-Jelszo VARCHAR(30) NOT NULL,
-FelhNev VARCHAR(50) NOT NULL UNIQUE,
-Email VARCHAR(100) NOT NULL );
-
-
--- Adatbázis/Lekérdezés #5.sql
+++
@@ -1,8 +0,0 @@
--- különböző streaming platformokat tartalmazó tábla
-CREATE TABLE StreamingPlatform(
-ID int AUTO_INCREMENT PRIMARY KEY,
-Nev VARCHAR(100) NOT NULL,
-LogoUrl VARCHAR(1000) NOT NULL,
-filmId INT,
-FOREIGN KEY (filmId) REFERENCES Film(ID) ON UPDATE CASCADE ON DELETE SET NULL  
-);
