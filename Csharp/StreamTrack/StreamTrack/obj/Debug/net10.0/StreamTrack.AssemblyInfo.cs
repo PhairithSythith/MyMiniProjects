53520dc4b15d1e3349563c947af6c1bd5f535615
PhairithSythith <jank69titanilla@gmail.com>
2026-06-02 10:27:57 +0200

    Automata terv-mentes (2026. 06. 02. 10:27:56,89)

--- Csharp/StreamTrack/StreamTrack/obj/Debug/net10.0/StreamTrack.AssemblyInfo.cs
+++ Csharp/StreamTrack/StreamTrack/obj/Debug/net10.0/StreamTrack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StreamTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9425c5b13a9e0d9729b633545e06a845231a0f92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79b14ea132170bfe6cd07ac4072c1099901ad25")]
 [assembly: System.Reflection.AssemblyProductAttribute("StreamTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StreamTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
