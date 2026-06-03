bd21f0151200f0e7fd6ffd14c0d1aedf59a85367
PhairithSythith <jank69titanilla@gmail.com>
2026-06-03 10:39:00 +0200

    Automata terv-mentes (2026. 06. 03. 10:39:00,28)

--- In_Progress_Projektek/To-do-list/To_Do/To_Do/wwwroot/javascript.js
+++ In_Progress_Projektek/To-do-list/To_Do/To_Do/wwwroot/javascript.js
@@ -1,4 +1,15 @@
 Function Render(){
     for (let i = 0; i < adatTarolo.length; i++) {
+        let feladat = adatTarolo[i].f;
+        let kulcsDate = adatTarolo[i].date;
+        let done = adatTarolo[i].done;
+        const $btnTorol = $('<button type="button">Törlés</button>');
+        const $btnKezs = $('<button type="button">Kész</button>');
+        const $nli = $('<li></li>').text(feladat);
+        const $ntarol = $('<div class="ntarol"></div>');
+        const $kli = $('<li></li>').text(feladat);
+        const $ktarol = $('<div class="ktarol"></div>');
+        const $btnArch = $('<button type="button">Archívumba mentés</button>');
+
     }
 };
