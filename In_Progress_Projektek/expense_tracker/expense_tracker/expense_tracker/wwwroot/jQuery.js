f0a852f7ba421c51363b9439634cc78350188862
PhairithSythith <jank69titanilla@gmail.com>
2026-06-08 15:12:07 +0200

    Automata terv-mentes (2026. 06. 08. 15:12:07,34)

--- In_Progress_Projektek/expense_tracker/expense_tracker/expense_tracker/wwwroot/jQuery.js
+++ In_Progress_Projektek/expense_tracker/expense_tracker/expense_tracker/wwwroot/jQuery.js
@@ -39,7 +39,7 @@ $(function () {
             $btnTorol.data("id", egyAdat.id);
             let $kTarolo = $('<div></div>');
             $kTarolo.addClass('kTarolo');
-            $kTarolo.append(`Költség típusa: ${beKi}, Megnevezés: ${vett}, Összeg: ${mennyi}, Időpont: ${mikor}`, $btnTorol);
+            $kTarolo.append(`Költség típusa: ${bekiSzin(beKi)}, Megnevezés: ${vett}, Összeg: ${mennyi}, Időpont: ${mikor}`, $btnTorol);
             $nTarolo.append($kTarolo);
         }
     };
